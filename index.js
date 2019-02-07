e44cf088b75a4ff85971f7ef826a3e00f5bc01ef
Daniel Gillett <daniel@danielgillett.com>
2019-02-07 22:02:33 +0000

    section 10 - 133. Storing Secrets in Environment Variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 // moved these two so we only have to reference it once
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -11,6 +12,11 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1); // anything but 0 (zero) is a fatal error
+}
 
 mongoose.connect('mongodb://localhost/vidly-app', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
