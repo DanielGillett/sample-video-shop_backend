0ad019a679a5cdf226e5277a91d8cfe6de9b3d93
Daniel Gillett <daniel@danielgillett.com>
2019-02-07 20:34:29 +0000

    section 10 .129 authenticating users

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
@@ -21,6 +22,7 @@ app.use('/api/customers/', customers);
 app.use('/api/movies/', movies);
 app.use('/api/rentals/', rentals);
 app.use('/api/users/', users);
+app.use('api/auth', auth);
 app.use('/', home);
 
 // Set or Export a port in terminal...
