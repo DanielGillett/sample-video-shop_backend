c877b33dbe180ab47b89aca590c08d39fde5f351
Daniel Gillett <daniel@danielgillett.com>
2019-02-11 06:39:11 +0000

    section 11 - 153. refactoring index.js - extracting routes

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 // moved these two so we only have to reference it once
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const home = require('./routes/home');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+
+
 const express = require('express');
 const app = express();
+require('./startup/routs')(app);
 
 // winston default, but good to know it's setup like this.
 winston.exitOnError = true;
@@ -55,15 +50,7 @@ mongoose.connect('mongodb://localhost/vidly-app', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-app.use(express.json());
-app.use('/api/genres/', genres);
-app.use('/api/customers/', customers);
-app.use('/api/movies/', movies);
-app.use('/api/rentals/', rentals);
-app.use('/api/users/', users);
-app.use('/api/auth', auth);
-app.use('/', home);
-app.use(error);
+
 
 // Set or Export a port in terminal...
 // BASH terminal ► export PORT=5000
